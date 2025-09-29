23444c92e3f4469514b89243a2d75a629a6d8d0e
G-Chandu-Balaji <balaji5220771@gmail.com>
2025-09-29 17:37:57 +0530

    intial basic setup for Frontend and In backend added server,coected to maogodb atlas database

---
+++ BackEnd/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = new express();
+let port = process.env.PORT || 5100;
+
+//Database connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => {
+    console.log("Database connection  failed", err.message);
+    process.exit(1);
+  });
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+//start server
+app.listen(port, () => {
+  console.log(`Server is runnning on ${port}...`);
+});
